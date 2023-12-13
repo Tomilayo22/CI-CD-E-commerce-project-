06476dcd6b8d828c8da9f718d36dc0024dc7ee10
Tomilayo22 <153629743+Tomilayo22@users.noreply.github.com>
2023-12-13 10:16:15 +0300

    Main project automated test suite.py

--- Main project automated test suite.py
+++ Main project automated test suite.py
@@ -3,15 +3,13 @@ import unittest
 
 # Define Test Classes
 class MyTestSuite(unittest.TestCase):
-
-    
+  
     def test_case_1(self):
-        """
-        Description: Test if True is equal to True.
-        """
+    """
+    Description: Test if True is equal to True.
+    """
         self.assertTrue(True)
 
-    
     def test_case_2(self):
         """
         Description: Test if the sum of 1 and 1 is equal to 2.
