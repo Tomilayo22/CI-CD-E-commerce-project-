0eae22df1adcc8e605576805b2d3845fb4659845
Tomilayo22 <153629743+Tomilayo22@users.noreply.github.com>
2023-12-13 10:29:35 +0300

    Update Test project.py

--- Test project.py
+++ Test project.py
@@ -23,8 +23,9 @@ class ShoppingCart:
         return total
 
 class ProductCatalog:
-    
     def __init__(self):
+
+        
         self.products = {}
 
     def add_product(self, product_id, name, price):
@@ -41,25 +42,28 @@ import unittest
 
 class TestECommerceComponents(unittest.TestCase):
     def setUp(self):
+
+        
         self.shopping_cart = ShoppingCart()
         self.product_catalog = ProductCatalog()
-
     def test_add_item_to_cart(self):
+
+        
         self.shopping_cart.add_item("product1", 2)
         self.assertEqual(self.shopping_cart.items["product1"], 2)
-
     def test_remove_item_from_cart(self):
         self.shopping_cart.add_item("product2", 3)
         self.shopping_cart.remove_item("product2", 1)
         self.assertEqual(self.shopping_cart.items["product2"], 2)
 
     def test_get_cart_total(self):
+
+        
         self.product_catalog.add_product("product3", "Item A", 10.0)
         self.product_catalog.add_product("product4", "Item B", 15.0)
         self.shopping_cart.add_item("product3", 2)
         self.shopping_cart.add_item("product4", 1)
         total = self.shopping_cart.get_cart_total()
         self.assertEqual(total, 35.0)
-
 if __name__ == '__main__':
     unittest.main()
