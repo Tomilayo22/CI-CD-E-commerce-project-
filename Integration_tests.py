73a33500f4190759c279b1c610893429137fd287
Tomilayo22 <153629743+Tomilayo22@users.noreply.github.com>
2023-12-13 10:58:26 +0300

    Update Integration_tests.py

--- Integration_tests.py
+++ Integration_tests.py
@@ -4,15 +4,15 @@ import unittest
 class MyTestSuite(unittest.TestCase):
     
     def test_case_1(self):
-    """
-    Description: Test if True is equal to True.
-    """
+        """
+        Description: Test if True is equal to True.
+        """
         self.assertTrue(True)
     
     def test_case_2(self):
         """
-    Description: Test if the sum of 1 and 1 is equal to 2.
-    """
+        Description: Test if the sum of 1 and 1 is equal to 2.
+        """
         self.assertEqual(1 + 1, 2)
 
 # Add more test classes as needed
